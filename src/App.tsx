e231e9150fd76b3979b7ad56496a5a20ace57a46
aliyarerbolatov12-maker <aliyarerbolatov12@gmail.com>
2025-11-11 23:26:41 +0500

    feat(routes): add Accordion route in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import ModalDemo from "./ui/modal/ModalDemo";
 import "./App.css";
 import TabsDemo from "./ui/tabs/TabsDemo";
 import DropdownDemo from "./ui/dropdown/DropdownDemo";
+import AccordionDemo from "./ui/accordion/AccordionDemo";
 
 export default function App() {
   return (
@@ -13,6 +14,7 @@ export default function App() {
           <Route path="modal" element={<ModalDemo />} />
           <Route path="tabs" element={<TabsDemo />} />
           <Route path="dropdown" element={<DropdownDemo />} />
+          <Route path="accordion" element={<AccordionDemo />} />
         </Route>
       </Routes>
     </BrowserRouter>
